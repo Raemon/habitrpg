7d3f57bc855f882d7708ff5c8d695fc692b82d15
Raemon <raemon777@gmail.com>
2015-01-12 15:15:11 -0500

    testing

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 /**
  * New Relic agent configuration.
- *
+ * test
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
